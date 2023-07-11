c653b72f3b7a79fa48877ec8e2f2592a73516e64
Vinay <vinay.g2404@gmail.com>
2023-07-11 23:22:54 +0530

    Adding DOM elements in JS

--- js/elements.js
+++ js/elements.js
@@ -71,3 +71,16 @@ console.log(footer);
 
 let footerstyle = (document.querySelector("footer").style.backgroundColor =
 	"purple");
+
+// Adding DOM Elements using JS
+
+let content = "This is the Bootcamp";
+let header = document.querySelector("header");
+let container = document.createElement("section");
+
+container.classList.add("pencil-box");
+container.setAttribute("id", "pencil");
+
+container.innerHTML = content;
+
+header.append(container);
