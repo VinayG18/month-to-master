f318835b13ef0adeaf49e354fc54c44cec54cf73
Vinay <vinay.g2404@gmail.com>
2023-07-12 23:41:15 +0530

    Objects in JS

---
+++ js/objects.js
@@ -0,0 +1,16 @@
+const pencilbox = {
+	name: "A Pencil Box",
+	size: 30,
+	color: "red",
+	zipLength: {
+		left: 12,
+		right: 12,
+		top: 24,
+	},
+	zipOpen: false,
+	toggleZip: function (zipStatus) {
+		this.zipOpen = zipStatus;
+	},
+};
+
+console.log(pencilbox);
