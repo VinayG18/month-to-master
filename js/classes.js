f75d246e55bb9339dcc53c2a0bdc59338bd5abc9
Vinay <vinay.g2404@gmail.com>
2023-07-13 23:37:52 +0530

    Displaying objects in Browser

--- js/classes.js
+++ js/classes.js
@@ -11,6 +11,22 @@ const newPencilBox = new pencilBox(
 	"2022-05-1 00:00:00"
 );
 
+const content = `
+    <main>
+        <ul>
+            <li>Name: ${newPencilBox.name}</li>
+            <li>Size: ${newPencilBox.size}</li>
+            <li>Color: ${newPencilBox.color}</li>
+            <li>Zip Length Left: ${newPencilBox.zipLength.left}</li>
+            <li>Zip Length Right: ${newPencilBox.zipLength.right}</li>
+            <li>Zip Length Top: ${newPencilBox.zipLength.top}</li>
+            <li class="status"> ${newPencilBox.zipOpen}</li>
+        </ul>
+    </main>
+`;
+
+document.body.innerHTML = content;
+
 console.log("The Pencil Box Object:", newPencilBox);
 console.log("Size:", newPencilBox.size);
 
