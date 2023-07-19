522d196797d56655c532daf41a6046f5228dcb56
Vinay <vinay.g2404@gmail.com>
2023-07-19 23:02:43 +0530

    The arrow function and this keyword in JS

--- js/functions.js
+++ js/functions.js
@@ -48,3 +48,22 @@ const addPencilBox = (currentPencilBox) => {
 
 document.body.append(addPencilBox(redpencilBox));
 document.body.append(addPencilBox(bluepencilBox));
+
+/* Arrow Function and this keyword */
+window.size = 13;
+
+const yellowpencilBox = {
+	name: "Yellow Pencil Box",
+	color: "blue",
+	size: 24,
+	newSize: function (size) {
+		console.log(this.size);
+		this.size = size;
+		console.log(this.size);
+		(() => {
+			console.log(this.size);
+		})();
+	},
+};
+
+console.log(yellowpencilBox.newSize(5));
